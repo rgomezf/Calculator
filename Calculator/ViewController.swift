84ac10cd455c06a584347ea046a9bb1dbaf1700a
Ramon A Gomez <ramon.gomez@noaa.gov>
2017-01-10 09:31:46 -0500

    First version of a calculator.  Added one operation.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: Properties
+    @IBOutlet weak var display: UILabel!
+    var userIsTyping = false
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //MARK: Actions
+    @IBAction func digitTouched(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if userIsTyping == true {
+            let currentDisplayedValue = display.text!
+            display.text = currentDisplayedValue + digit
+        } else {
+            display.text = digit
+        }
+        userIsTyping = true
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func performOperation(sender: UIButton) {
+        userIsTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            if mathematicalSymbol == "π" {
+                display.text = String(M_PI)
+            }
+        }
     }
-
-
 }
 
